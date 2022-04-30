d38a33d350c246383a66b0edbb670e99834c8b1b
toshihirock <toshihiro308@gmail.com>
2022-04-30 12:40:45 +0900

    initial commit

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -9,7 +9,11 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('toshhirock/cdk-pipelin', 'main'),
+        //input: CodePipelineSource.gitHub('toshhirock/cdk-pipeline', 'main'),
+
+        input: CodePipelineSource.connection('toshihirock/cdk-pipeline', 'master', {
+          connectionArn: 'arn:aws:codestar-connections:ap-northeast-1:323817733012:connection/1e20feb9-f336-40eb-a9ad-018ee518332f',
+        }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
